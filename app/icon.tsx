f617fd904a28d0ceb149b2b1dfe3f9ae50f03da2
Germária Lins <germarialins@hotmail.com>
2025-12-06 03:00:11 -0300

    Ajustes para o deploy

--- app/icon.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-export const runtime = "edge"
-
-export function GET(request: Request) {
-  const url = new URL("/logo.png", request.url)
-  return NextResponse.redirect(url)
-}
